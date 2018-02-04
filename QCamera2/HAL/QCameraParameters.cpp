sonyxperiadev/camera

Qcamera2: Send DIS parameter with recording hint
DIS parameter needs to be set late as IS thread will not be
created before stream starts.

CRs-fixed: 716275
Change-Id: I3a11f90bd23c38979e903fca9c169e8eb6be98df

QCamera2/HAL/QCameraParameters.cpp
@@ -3354,6 +3354,10 @@ int32_t QCameraParameters::setRecordingHint(const QCameraParameters& params)
                 if (getFaceDetectionOption() == true) {
                     setFaceDetection(value > 0 ? false : true, false);
                 }
+                if (m_bDISEnabled) {
+                    CDBG_HIGH("%s: %d: Setting DIS value again", __func__, __LINE__);
+                    setDISValue(VALUE_ENABLE);
+                }
                 return NO_ERROR;
             } else {
                 ALOGE("Invalid recording hint value: %s", str);
@@ -5758,7 +5762,7 @@ int32_t QCameraParameters::setDISValue(const char *disStr)
             //preview because of topology change in backend. But, for now, restart preview
             //for all IS types.
             m_bNeedRestart = true;
-            CDBG("%s: Setting DIS value %s", __func__, disStr);
+            CDBG_HIGH("%s: Setting DIS value %s", __func__, disStr);
             updateParamEntry(KEY_QC_DIS, disStr);
             if (!(strcmp(disStr,"enable"))) {
                 m_bDISEnabled = true;
@@ -8155,6 +8159,11 @@ int32_t QCameraParameters::updateRecordingHintValue(int32_t value)
     if (rc != NO_ERROR) {
         ALOGE("%s:Failed to update table", __func__);
         return rc;
+    }
+
+    if(m_bDISEnabled && (value==1)) {
+        CDBG_HIGH("%s: %d: Setting DIS value again!!", __func__, __LINE__);
+        setDISValue(VALUE_ENABLE);
     }
 
     rc = commitSetBatch();

nsytechno/hardware_qcom_camera

QCamera2/HAL/QCameraParameters.cpp
@@ -3358,6 +3358,10 @@ int32_t QCameraParameters::setRecordingHint(const QCameraParameters& params)
             if(value != NAME_NOT_FOUND){
                 updateParamEntry(KEY_RECORDING_HINT, str);
                 setRecordingHintValue(value);
+                if (m_bDISEnabled) {
+                    ALOGE("%s: %d: Setting DIS value again", __func__, __LINE__);
+                    setDISValue(VALUE_ENABLE);
+                }
                 return NO_ERROR;
             } else {
                 ALOGE("Invalid recording hint value: %s", str);
@@ -5659,7 +5663,7 @@ int32_t QCameraParameters::setDISValue(const char *disStr)
             //preview because of topology change in backend. But, for now, restart preview
             //for all IS types.
             m_bNeedRestart = true;
-            ALOGD("%s: Setting DIS value %s", __func__, disStr);
+            ALOGE("%s: Setting DIS value %s", __func__, disStr);
             updateParamEntry(KEY_QC_DIS, disStr);
             if (!(strcmp(disStr,"enable"))) {
                 m_bDISEnabled = true;
@@ -7868,6 +7872,11 @@ int32_t QCameraParameters::updateRecordingHintValue(int32_t value)
     if (rc != NO_ERROR) {
         ALOGE("%s:Failed to update table", __func__);
         return rc;
+    }
+
+    if(m_bDISEnabled && (value==1)) {
+        ALOGE("%s: %d: Setting DIS value again!!", __func__, __LINE__);
+        setDISValue(VALUE_ENABLE);
     }
 
     rc = commitSetBatch();